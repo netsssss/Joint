0c946ab308d39c2a4f3ed619b49b90818ca2c6fa
netsssss <1543482826@qq.com>
2019-04-09 10:19:58 +0800

    update

---
+++ joint.js
@@ -0,0 +1,37 @@
+"use strict";
+/**
+ * update on 2019/4/8
+ * create from Xu
+ */
+var joint = {
+    add: function (name, param) {
+        if (!this.get(name)) {
+            var theParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(param));
+            window.location.search += window.location.search ? '&' + theParam : '?' + theParam;
+        }
+    },
+    remove: function (name) {
+        if (this.get(name)) {
+            var temp = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(this.get(name)));
+            var params = window.location.search.indexOf(temp) > -1 ? 
+                window.location.search.replace((window.location.search.substring(window.location.search.indexOf(temp)-1, window.location.search.indexOf(temp))) + temp, '') : '';
+            params ? window.location.search = '?' + params.substr(1) : window.location = window.location.pathname;
+        }
+    },
+    replace: function (name, param) {
+        if (this.get(name)) {
+            var oldParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(this.get(name)));
+            var newParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(param));
+            var params = window.location.search.indexOf(oldParam) > -1 ? window.location.search.replace(oldParam, newParam) : '';
+            window.location.search = params ?  params : window.location.pathname;
+        }
+    },
+    get: function (name) {
+        var reg = new RegExp("(^|&)" + encodeURI(encodeURI(name)) + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(unescape(r[2])); return null;
+    },
+    clear: function () {
+        window.location = window.location.pathname;
+    }
+}
---
+++ joint2.js
@@ -0,0 +1,51 @@
+"use strict";
+/**
+ * update on 2019/4/8
+ * create from Xu
+ */
+var joint2 = {
+    add: function (name, param) {
+        if (!this.get(name)) {
+            var theParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(param));
+            this._init();
+            this._search += this._search ? this._search == '#' ? theParam : '&' + theParam : '#' + theParam;
+            this._join();
+        }
+    },
+    remove: function (name) {
+        if (this.get(name)) {
+            var temp = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(this.get(name)));
+            this._init();
+            var params = this._search.indexOf(temp) > -1 ? this._search.replace((this._search.substring(this._search.indexOf(temp)-1, this._search.indexOf(temp))) + temp, '') : '';
+            this._search = params ? '#' + params.substr(1) : '#';
+            this._join();
+        }
+    },
+    replace: function (name, param) {
+        if (this.get(name)) {
+            var oldParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(this.get(name)));
+            var newParam = encodeURI(encodeURI(name)) + '=' + encodeURI(encodeURI(param));
+            this._init()
+            this._search = this._search.indexOf(oldParam) > -1 ? this._search.replace(oldParam, newParam) : '';
+            this._join();
+        }
+    },
+    get: function (name) {
+        var reg = new RegExp("(^|&)" + encodeURI(encodeURI(name)) + "=([^&]*)(&|$)");
+        var r = window.location.href.substr(window.location.href.indexOf('#') + 1).match(reg);
+        if (r != null) return decodeURI(unescape(r[2])); return null;
+    },
+    clear: function () {
+        this._search = '#';
+        this._join();
+    },
+    _init: function () {
+        var num = window.location.href.indexOf('#') > -1 ? window.location.href.indexOf('#') : window.location.href.length;
+        this._search = window.location.href.substr(num);
+    },
+    _join: function () {
+        var num = window.location.href.indexOf('#') > -1 ? window.location.href.indexOf('#') : window.location.href.length;
+        window.location.href = window.location.href.substring(0, num) + this._search;
+    },
+    _search: ''
+}
